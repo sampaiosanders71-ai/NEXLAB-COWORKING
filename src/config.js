34b88c6a12a651524ee51d08c15efcdc3a0c1c2f
sampaiosanders71-ai <sampaiosanders71@gmail.com>
2026-07-02 21:03:19 -0300

    Delete src directory

--- src/config.js
+++
@@ -1,6 +0,0 @@
-globalThis.__NEXLAB_CONFIG__ = {
-  "supabaseUrl": "https://eahldhabwulnwhuwrhvc.supabase.co",
-  "supabaseAnonKey": "sb_publishable_hr-WTQUBbBE0Ei3Lr2hkhQ_XSKG_PXa",
-  "vapidPublicKey": "BC-C9g9bRHzNTrffU4ffaG_z2iay3zO16Xe4bL93k6JTT2Z2OlJm3q4RR_Rjt8vkSAnG92B4_3s2rHT9DiDVb40",
-  "appUrl": ""
-};
